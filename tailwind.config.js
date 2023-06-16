7a2eda2dbb21bee589ac2956a7af98d423d9d3e7
albi <aruchevy@gmail.com>
2023-06-16 21:01:28 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['Index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#075985',
+        facebook: '#3b5998'
+      },
+      screen: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
+
